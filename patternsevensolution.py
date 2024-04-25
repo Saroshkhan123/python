c5e865f200406035d48ca273dd03baaa40632ea5
Saroshkhan123 <167165084+Saroshkhan123@users.noreply.github.com>
2024-04-25 10:47:16 +0530

    patternsevensolution.py

---
+++ patternsevensolution.py
@@ -0,0 +1,14 @@
+#Pattern-Seven solution
+
+n = int(input())
+spaces = n - 1
+for row in range(1, n + 1):
+      
+   for space in range(spaces):
+       print(" ", end = "")
+   spaces -= 1
+  
+   for col in range(1, row + 1):
+       print(col, end = "")
+   print()
+
